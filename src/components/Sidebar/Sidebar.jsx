ad50d314185a64a2ee269587c22784a0d929f663
masatosotoma <masatosotoma@gmail.com>
2024-09-28 22:07:19 -0400

    Added CSS rules for sidebar img,menu,new-chat

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,9 +6,33 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" />
+        <img className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <dov className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react...</p>
+          </dov>
+        </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Setting</p>
+        </div>
+      </div>
     </div>
   );
 };
