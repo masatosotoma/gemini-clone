2133c0b4b6b85418ed94006b64f47b86f8944689
masatosotoma <masatosotoma@gmail.com>
2024-10-01 22:57:04 -0400

    Added the useState for new chat and recent chat

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="top">
         <img className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title">Recent</p>
-          <dov className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>What is react...</p>
-          </dov>
-        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react...</p>
+            </div>
+          </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
