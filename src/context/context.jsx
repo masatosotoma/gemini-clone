8e0578865adb8f9ecbef428627056297105d2a47
masatosotoma <masatosotoma@gmail.com>
2024-10-10 22:23:31 -0400

    added all the states in context

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { createContext } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
   const onSent = async (prompt) => {
     await run(prompt);
   };
-
-  onSent("what is react js");
 
   const contextValue = {};
 
