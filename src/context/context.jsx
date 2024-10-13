920346672e300f5fc5f66018cff11401134a0b15
masatosotoma <masatosotoma@gmail.com>
2024-10-12 22:06:31 -0400

    Added paragraph delay parameter function

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -12,7 +12,11 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
-  const delayPara = (index, nextWord) => {};
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
 
   const onSent = async (prompt) => {
     setResultData("");
@@ -29,7 +33,12 @@ const ContextProvider = (props) => {
         newResponse += "<b>" + responseArray[i] + "</b>";
       }
     }
-    setResultData(newResponse);
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord+" ");
+    }
     setLoading(false);
     setInput("");
   };
