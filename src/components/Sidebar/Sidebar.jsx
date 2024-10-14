e158f1c29dcad6c0cf2024348437e0b857fcc41c
masatosotoma <masatosotoma@gmail.com>
2024-10-14 18:19:35 -0400

    Made it clickable for the previous search history

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,6 +7,11 @@ const Sidebar = () => {
   const [extended, setExtended] = useState(false);
   const { onSent, prevPrompts, setRecentPrompt } = useContext(Context);
 
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -27,7 +32,7 @@ const Sidebar = () => {
             <p className="recent-title">Recent</p>
             {prevPrompts.map((item, index) => {
               return (
-                <div className="recent-entry">
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
                   <img src={assets.message_icon} alt="" />
                   <p>{item.slice(0, 18)}...</p>
                 </div>
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -22,9 +22,16 @@ const ContextProvider = (props) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPrevPrompts((prev) => [...prev, input]);
-    const response = await run(input);
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompts((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await run(input);
+    }
+
     let responseArray = response.split("**");
     let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
